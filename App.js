1ab4750bfee5793848710caa49b44f9e6686cd5d
shahrukh-khaliq <shahrukhkhaliq422@gmail.com>
2024-05-11 07:57:15 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+"use strict";
+let obtainedMarks = 400;
+let TotalMarks = 850;
+let per = obtainedMarks * 100 / TotalMarks;
+if (per >= 80 && per <= 100) {
+    console.log(`your grade is A1 and your percentage is ${per}`);
+}
+else if (per >= 70 && per <= 80) {
+    console.log(`your grade is A and your percentage i ${per}`);
+}
+else if (per >= 60 && per <= 70) {
+    console.log(`your grade is B and your percentage is ${per}`);
+}
+else if (per >= 50 && per <= 60) {
+    console.log(`your grade is C and your percentage is ${per}`);
+}
+else {
+    console.log(`you are passed ${per}`);
+}
